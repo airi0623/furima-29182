4a6aedea30a7d49d59f29b0905eb679719f4cc22
airi0623 <marcy.0623.doi@gmail.com>
2020-09-05 19:04:51 +0900

    モデルテスト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_one    :order
   with_options presence: true do
     validates :nickname
+    #validates :email
     #validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/, message: "",""}
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,3 @@
-Faker::Config.locale = :ja
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.free_email}
@@ -6,8 +5,8 @@ FactoryBot.define do
     password {password}
     password_confirmation {password}
     nickname { Faker::Name.last_name }
-    family_name { Faker::Name.last_name }
-    first_name { Faker::Name.first_name }
+    family_name { Gimei.last.kanji }
+    first_name { Gimei.first.kanji }
     family_name_kana { Gimei.last.katakana }
     first_name_kana { Gimei.first.katakana }
     birth_date { Faker::Date.birthday }
