9e551445012f727212b4a0860dfe24cad56731f8
airi0623 <marcy.0623.doi@gmail.com>
2020-09-06 16:00:52 +0900

    itemsのmigrationファイル修正

--- db/migrate/20200906023323_create_items.rb
+++ db/migrate/20200906023323_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :item_name,     null: false
-      t.integer :price,        null: false
+      #t.integer :price,        null: false
       t.string :detail,        null: false
       t.references :user,      null: false,foreign_key: true
       t.integer :category_id,            null: false
