c3546cae042f4722802c29a975a4584ff233c8e6
airi0623 <marcy.0623.doi@gmail.com>
2020-09-06 16:02:02 +0900

    ActiveHash_カテゴリー

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_09_06_054609) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
-    t.integer "price", null: false
     t.string "detail", null: false
     t.bigint "user_id", null: false
     t.integer "category_id", null: false
