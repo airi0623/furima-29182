148fe7eb78cab02a977a367451a6a924b52c41b9
airi0623 <marcy.0623.doi@gmail.com>
2020-09-10 21:04:52 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show,:update]
+  before_action :set_item, only: [:edit, :show,:update,:destroy]
 
 
   def index
@@ -33,6 +33,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
 
   private
   
