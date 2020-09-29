b4928115ce0f0715fd9dfa61f5c54706b189d011
airi0623 <marcy.0623.doi@gmail.com>
2020-09-29 21:30:01 +0900

    プルダウンメニューデザイン実装

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -4,21 +4,37 @@ window.addEventListener("load", () => {
   const pullDownCategory = document.getElementById("pull-down-category");
   const categoryLists = document.querySelectorAll(".category-list");
   
-  
   // カテゴリーボタン、リスト表示の有無
   categoryBtn.addEventListener('mouseover', function(){
-    categoryBtn.setAttribute("style", "background-color:#3ccace; color:#FFF;");
+    categoryBtn.setAttribute("style", " color:#3ccace;");
   })
   categoryBtn.addEventListener('mouseout', function(){
-    categoryBtn.removeAttribute("style", "background-color:#3ccace; color:#FFF;");
+    categoryBtn.removeAttribute("style", "color:#3ccace;");
   })
-  categoryBtn.addEventListener('mouseover', function() {
-    if (pullDownCategory.getAttribute("style") == "display:inline;"){
-      pullDownCategory.removeAttribute("style", "display:inline;")
-    } else {
-      pullDownCategory.setAttribute("style", "display:inline;")
-    }
+
+  // categoryBtn.addEventListener('mouseover', function() {
+  //   if (pullDownCategory.getAttribute("style") == "display:inline;"){
+  //     pullDownCategory.removeAttribute("style", "display:inline;")
+  //   } else {
+  //     pullDownCategory.setAttribute("style", "display:inline;")
+  //   }
+  // })
+
+  // pullDownCategory.addEventListener('mouseout', function() {
+  //   if (pullDownCategory.getAttribute("style") == "display:inline;"){
+  //     pullDownCategory.removeAttribute("style", "display:inline;")
+  //   } 
+  // })
+
+  document.querySelector('.lists-left').addEventListener('mouseover', function() {
+    pullDownCategory.setAttribute("style", "display:block;")
   })
+  
+  // categoryLists.forEach(function(element){
+    document.querySelector('.lists-left').addEventListener('mouseout', function() {
+      pullDownCategory.setAttribute("style", "display:none;")
+    })
+  // })
 
   // ボタンカラー
   categoryLists.forEach(function(element){
