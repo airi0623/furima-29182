2ee560d619957064d9998fe112c7c5c2e918ac3c
airi0623 <marcy.0623.doi@gmail.com>
2020-10-02 12:22:57 +0900

    お気に入りの商品-表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id]).order("created_at DESC")
+    
+    like = Like.where(user_id: current_user.id)
+
+    
+    # @like_items = Item.where(params:[item_id]) 
+    #@like_items = Item.find(params[:id][]) wrong number of arguments (given 0, expected 1..2)
+    #@like_items = Item.find(id: :item_id) Couldn't find Item with 'id'={:id=>:item_id}
+    #@like_items = Item.find(item_id) undefined local variable or method `item_id' for #<UsersController:0x00007fa108b60890> Did you mean? item_url
+    #@like_items = Item.where(params:[item_id]) undefined local variable or method `item_id' for #<UsersController:0x00007fa108f28f68> Did you mean? item_url
+    #@like_items = Item.find(like.item_id)
   end
 
 
