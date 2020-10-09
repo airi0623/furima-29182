8309a99e677579a111f186fcf41a75e2fd8c6d9b
airi0623 <marcy.0623.doi@gmail.com>
2020-10-09 19:06:18 +0900

    お気に入り機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,22 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @items = Item.all.sort {|a,b| b.likes.count <=> a.likes.count}
+    @categorys = Category.where(id: 2..10)
+  end
+
   def create
-    user = current_user
+
+    user = current_user.id
     @item = Item.find(params[:item_id])
-    Like.create!(user_id: @item.user_id, item_id: @item.id)
+    Like.create!(user_id: user, item_id: @item.id)
     #render 'create.js.erb'
   end
 
   def destroy
     @item = Item.find(params[:item_id])  
-    like = Like.find_by(user_id: @item.user_id, item_id: @item.id )
+    like = Like.find_by(user_id: current_user.id, item_id: @item.id )
     like.delete
     #render 'destroy.js.erb'
   end
